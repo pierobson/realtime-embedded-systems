c146ca1d1bbaf9400d2e293544738db1c88022fc
Pierce Robson <piercerobson@gmail.com>
2023-09-24 14:13:43 -0700

    Record temps for 12 mins

--- temperature/temp-recorder/src/main.rs
+++ temperature/temp-recorder/src/main.rs
@@ -28,9 +28,9 @@ fn main() -> Result<(), Box<dyn Error>> {
     // Print headers to the CSV file
     wtr.write_record(["Timestamp (ms)", "Temperature (°F)"])?;
 
-    // Record data for 10 minutes
+    // Record data for 12 minutes
     let start_time = SystemTime::now();
-    let duration = Duration::from_secs(600); // 10 minutes
+    let duration = Duration::from_secs(720); // 12 minutes
     let mut buffer: Vec<u8> = Vec::new();
     let mut read_buffer: [u8; 128] = [0; 128];
 
